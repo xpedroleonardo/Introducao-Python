8062665f43d5ca68b7a321559ab611842ca887d8
xpedroleonardo <aispedroleonardo@gmail.com>
2021-02-05 15:22:10 -0300

    first commit

---
+++ introducao.py
@@ -0,0 +1,22 @@
+# Código utilizado para o python entender os caracteres especiais
+# -*- coding: utf-8 -*-
+# Nota: O código deve ficar comentado para funcionar
+
+# Exibe uma mensagem na tela: print()
+
+# print("Olá Mundo !!!")
+
+# Operadores Lógicos: and or not
+
+# Estrutura condicional: IF, IF ESLSE, ELSEIF
+
+x = 2
+y = 1
+
+# if x > y:
+#   print("x é maior")
+
+# if y > x:
+#   print("y é maior")
+# else:
+#   print("x é maior")
