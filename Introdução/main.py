4bccf958ad8f5f8ba4ed997f0fc9cc2dfdd72ef7
xpedroleonardo <aispedroleonardo@gmail.com>
2021-02-08 23:06:22 -0300

    functions, files and lists (array)

--- Introdução/main.py
+++ Introdução/main.py
@@ -92,4 +92,87 @@ text = "Olá Mundo "
 
 ## SUBSTITUIR UM VALOR DA STRING: .replace()
 
-print(text.replace("Mundo","World"))
+# print(text.replace("Mundo","World"))
+
+## FUNÇÕES: def
+
+# def soma(x, y):
+#   print(x+y)
+
+# soma(1,2)
+
+## MANIPULAÇÃO DE ARQUIVOS
+
+## ABRINDO ARQUIVOS: open(nome, modo)
+
+# arquivo = open("./Introdução/arquivo.txt")
+
+## LENDO AQUIVOS: .read(), .readline() e .readlines()
+
+# textoArquivo = arquivo.read()
+# print(textoArquivo)
+
+# linhaArquivo = arquivo.readline()
+# print(linhaArquivo)
+
+# linhasArquivo = arquivo.readlines()
+# print(linhasArquivo)
+
+## CRIANDO AQUIVO: open("...","w"), .write() e .close()
+
+# criando = open("./Introdução/criando.txt", "w")
+
+# criando.write("Esse é o arquivo Criado!!!")
+
+# criando.close()
+
+## Listas (ARRAY)
+
+## .append() : adiciona um valor no final
+
+# a.append(7)
+# print(a)
+
+## VERIFICANDO ITENS NA LISTA
+
+# if 1 in a:
+#   print("Esse valor está na lista")
+
+## REMOVENDO ITENS DA LISTA 
+
+# del a[0:3] #posição que começa e termina de apagar
+# print(a)
+
+# del a[:] #apaga tudo da lista
+# print(a)
+
+## ORDENANDO A LISTA
+
+# a.sort()  # Crescente, altenado a original
+# print(a)
+
+# a = sorted(a)  # Crescente, não altenado a original
+# print(a)
+
+# a.sort(reverse = True)  # Decrecente, altenado a original
+# print(a)
+
+# a.reverse() # Reverte a posição dos itens da lista
+# print(a)
+
+
+## DICIONÁRIOS (OBJETO)
+
+# dados = {
+#   "Nome": "Pedro Leonardo",
+#   "GitHub": "xpedroleonardo",
+#   "Repository": "Python"
+# }
+
+# print(dados["Nome"])  # Exibir um valor
+
+# for d in dados.values(): # Exibir todos os valores
+#   print(d) 
+
+# for d in dados.items():  # Exibe a chave e o valor
+#   print(d)
